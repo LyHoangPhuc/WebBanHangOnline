3461e5810d49a9d926358fc4e17c1a0a4a3f2e5c
LyHoangPhuc <146864195+LyHoangPhuc@users.noreply.github.com>
2024-03-14 18:27:18 +0700

    git init

---
+++ WebBanHangOnline/WebBanHangOnline/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            var items = db.Categories;
+            return View(items);
+        }
+    }
+}
